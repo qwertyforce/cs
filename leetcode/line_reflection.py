893e6d830a27d4ba40e96df17ae9581f1ae40ebc
qwertyforce <qwertyforce@protonmail.com>
2023-05-02 11:02:25 +0300

    Update line_reflection.py

--- leetcode/line_reflection.py
+++ leetcode/line_reflection.py
@@ -4,7 +4,7 @@ for x, y in points:
     min_x = min(min_x, x)
     max_x = max(max_x, x)
     point_set.add((x, y))
-s = min_x + max_x
+s = (min_x + max_x)/2
 for x, y in points:
-    if (s - x, y) not in point_set:
+    if (2*s-x, y) not in point_set:
         return False
